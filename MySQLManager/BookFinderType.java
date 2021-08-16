58b97923073b0225e282d636db1dbeab00c26a66
YYYork <80703116+YYYork@users.noreply.github.com>
2021-08-16 13:41:20 +0800

    Delete MySQLManager directory

--- MySQLManager/BookFinderType.java
+++
@@ -1,5 +0,0 @@
-package MySQLManager;
-
-public enum BookFinderType {
-	SEARCH_FOR_NAME,SEARCH_FOR_TYPE,SEARCH_FOR_PUBLISHER
-}
--- MySQLManager/RegisterState.java
+++
@@ -1,5 +0,0 @@
-package MySQLManager;
-
-public enum RegisterState{
-	 ACCOUNT_SUCCESS_REGISTER,ACCOUNT_EXITS,UNKNOWN_EXCEPTION
-}
