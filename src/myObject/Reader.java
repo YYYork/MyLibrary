2aad987e72a459df2e9ca866caafc2546acccf2b
YYYork <80703116+YYYork@users.noreply.github.com>
2021-08-17 16:34:48 +0800

    Update Reader.java

--- src/myObject/Reader.java
+++ src/myObject/Reader.java
@@ -3,8 +3,19 @@ package myObject;
 public class Reader {
 	private String account;
 	private String name;
+	private boolean isAdmin;
 	
-	
+	public Reader(String account, String name, boolean isAdmin) {
+		super();
+		this.account = account;
+		this.name = name;
+		this.isAdmin = isAdmin;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
 	public Reader(String account, String name) {
 		super();
 		this.account = account;
@@ -14,6 +25,7 @@ public class Reader {
 	public String getAccount() {
 		return account;
 	}
+	
 	public String getName() {
 		return name;
 	}
